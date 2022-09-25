bdd2c0cd7ce76f29dca3693bbbc3f568b52f8c9c
harendra2691 <harendrasingh2691@gmail.com>
2022-09-26 00:07:27 +0530

    update

---
+++ index.js
@@ -0,0 +1,68 @@
+const validation = () => {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const password = document.getElementById("password").value;
+  const confirm_password = document.getElementById("confirm-password").value;
+
+  document.getElementById("name-error").innerHTML = "";
+  document.getElementById("email-error").innerHTML = "";
+  document.getElementById("phone-error").innerHTML = "";
+  document.getElementById("password-error").innerHTML = "";
+  document.getElementById("confirm-password-error").innerHTML = "";
+
+  if (name == "") {
+    document.getElementById("name-error").innerHTML = "Username is required";
+    return false;
+  }
+  if (name.length <= 2 || name.length > 20) {
+    document.getElementById("name-error").innerHTML =
+      "Username range is 3 to 20";
+    return false;
+  }
+  if (!isNaN(name)) {
+    document.getElementById("name-error").innerHTML = "Use characters";
+    return false;
+  }
+
+  if (email == "") {
+    document.getElementById("email-error").innerHTML = "Email is required";
+    return false;
+  }
+
+  if (phone == "") {
+    document.getElementById("phone-error").innerHTML =
+      "Phone Number is required";
+    return false;
+  }
+  if (isNaN(phone)) {
+    document.getElementById("phone-error").innerHTML = "Use numbers only";
+    return false;
+  }
+  if (phone.length != 10) {
+    document.getElementById("phone-error").innerHTML = "Ten digits required";
+    return false;
+  }
+
+  if (password == "") {
+    document.getElementById("password-error").innerHTML =
+      "Password is required";
+    return false;
+  }
+  if (password.length <= 5 || name.length > 20) {
+    document.getElementById("password-error").innerHTML =
+      "Password must be in between 5-20";
+    return false;
+  }
+
+  if (confirm_password == "") {
+    document.getElementById("confirm-password-error").innerHTML =
+      "Please confirm the password";
+    return false;
+  }
+  if (password != confirm_password) {
+    document.getElementById("confirm-password-error").innerHTML =
+      "password not matching";
+    return false;
+  }
+};
